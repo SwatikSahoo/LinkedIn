88b76c6228e8ea8ec59d7e023a3aa9153a392d87
SWATIK\syank <syanki007@gmail.com>
2025-05-12 19:43:33 +0530

    changes

---
+++ src/Profile.java
@@ -0,0 +1,58 @@
+import java.util.List;
+
+public class Profile {
+    private String profilePicture;
+    private String headline;
+    private String summary;
+    private List<Experience> experiences;
+    private List<Education> educations;
+    private List<Skill> skills;
+
+    public String getProfilePicture() {
+        return profilePicture;
+    }
+
+    public void setProfilePicture(String profilePicture) {
+        this.profilePicture = profilePicture;
+    }
+
+    public String getHeadline() {
+        return headline;
+    }
+
+    public void setHeadline(String headline) {
+        this.headline = headline;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public List<Experience> getExperiences() {
+        return experiences;
+    }
+
+    public void setExperiences(List<Experience> experiences) {
+        this.experiences = experiences;
+    }
+
+    public List<Education> getEducations() {
+        return educations;
+    }
+
+    public void setEducations(List<Education> educations) {
+        this.educations = educations;
+    }
+
+    public List<Skill> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(List<Skill> skills) {
+        this.skills = skills;
+    }
+}
